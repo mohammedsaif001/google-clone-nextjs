8fe8caa5388b1c9de44dc382d2533022e2767f16
mohammedsaif-openturf <mohammed.saif@openturf.in>
2023-07-31 19:20:14 +0530

    Search Results

--- app/[...search]/page.js
+++ app/[...search]/page.js
@@ -1,28 +1,23 @@
 import getResults from "@/lib/getResults"
 import HeaderSearch from "../components/HeaderSearch"
+import SearchResults from "../components/SearchResults"
 
 const SearchPage = async ({ params, searchParams: { term } }) => {
     const data = await getResults(term)
-    console.log(data)
     return (
         <div>
             <HeaderSearch />
+            <SearchResults results={data} />
         </div>
     )
 }
+
+export async function generateMetadata({ params, searchParams }) {
+    const term = searchParams.term
+    const capitalizeTerm = term.charAt(0).toUpperCase() + term.slice(1);
+    return {
+        title: `${capitalizeTerm} - Search Results`
+    }
+}
+
 export default SearchPage
-
-// export async function getServerSideProps(context) {
-//     const query = context.query
-//     const useDummyData = false;
-//     const data = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${query.term}`).then(res => res.json())
-
-//     // After the Server rendering pass the results to the client
-
-//     return {
-//         props: {
-//             results: data,
-//             name: "Mohammed"
-//         }
-//     }
-// }
--- app/search/layout.js
+++
@@ -1,9 +0,0 @@
-export const metadata = {
-  title: 'Search For',
-}
-
-export default function RootLayout({ children }) {
-  return (
-    <>{children}</>
-  )
-}
