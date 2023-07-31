2117e902d59281b5bbbf06e4b892185f68ccb650
mohammedsaif-openturf <mohammed.saif@openturf.in>
2023-07-31 19:39:38 +0530

    Pagination

---
+++ app/components/Pagination.js
@@ -0,0 +1,33 @@
+"use client";
+import Link from "next/link";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
+import { useSearchParams } from "next/navigation";
+
+const Pagination = () => {
+    const searchParams = useSearchParams();
+    const start = searchParams.get("start")
+    const term = searchParams.get("term")
+
+    const startIndex = Number(start) || 0;
+
+    return (
+        <div className="flex max-w-lg justify-between text-blue-700 mb-10">
+            {startIndex >= 10 && (
+                <Link href={`/search?term=${term}&start=${startIndex - 10}`}>
+                    <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
+                        <ChevronLeftIcon className="h-5" />
+                        <p>Previous</p>
+                    </div>
+                </Link>
+            )}
+
+            <Link href={`/search?term=${term}&start=${startIndex + 10}`}>
+                <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
+                    <ChevronRightIcon className="h-5" />
+                    <p>Next</p>
+                </div>
+            </Link>
+        </div>
+    );
+};
+export default Pagination;
--- app/components/SearchResults.js
+++ app/components/SearchResults.js
@@ -1,3 +1,5 @@
+import Pagination from "./Pagination"
+
 const SearchResults = ({ results }) => {
     return (
         <div className="mx-auto w-full px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
@@ -17,6 +19,8 @@ const SearchResults = ({ results }) => {
                     <p className="line-clamp-2">{result.snippet}</p>
                 </div>
             ))}
+
+            <Pagination />
         </div>
     )
 }
